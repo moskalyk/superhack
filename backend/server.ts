b986c40b642865bf0c58068db9f9f252891357a7
moskalyk <morgan.moskalyk@protonmail.ch>
2023-08-08 18:11:11 -0400

    add beginning of bridge

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import {ethers, utils} from 'ethers'
+import { SequenceIndexerClient } from '@0xsequence/indexer'
+
 const pkey = process.env.pkey!
 const wallet = new ethers.Wallet(pkey);
 const PORT = process.env.PORT || 4000
@@ -10,8 +12,10 @@ const app = express();
 
 const provider = new ethers.providers.JsonRpcProvider('https://nodes.sequence.app/optimism');
 
+const indexer = new SequenceIndexerClient('https://optimism-indexer.sequence.app')
+
 const corsOptions = {
-    origin: ['https://round-mouse-4296.on.fleek.co', 'http://localhost:3001'],
+    origin: ['http://localhost:3000'],
 };
   
 app.use(cors(corsOptions));
@@ -38,7 +42,7 @@ async function loginAndGetData(email, password) {
     returnSecureToken: true,
   });
 
-  try {
+  // try {
     const response = await fetch(url, {
       method: 'POST',
       headers,
@@ -53,9 +57,9 @@ async function loginAndGetData(email, password) {
     user_access_token = data.idToken;
     user_id = data.localId;
     return await fetchData(user_id);
-  } catch (error) {
-    console.error('Error logging in:', email, error.message);
-  }
+  // } catch (error) {
+    // console.error('Error logging in:', email, error.message);
+  // }
 }
 
 function normalizeArray(arr) {
@@ -72,11 +76,11 @@ function normalizeArray(arr) {
 }
   
 async function fetchData(user_id: any) {
-    var startTimeVal = Date.now()/1000.0 - 120.0 * 60.0; //default to last 60 minutes of data
-    var endTimeVal = Date.now()/1000.0;
-    var query_start_time = startTimeVal;
-    var query_end_time = endTimeVal;
-    var numberOfDocuments = 10;
+  var startTimeVal = Date.now()/1000.0 - 120.0 * 60.0; //default to last 60 minutes of data
+  var endTimeVal = Date.now()/1000.0;
+  var query_start_time = startTimeVal;
+  var query_end_time = endTimeVal;
+  var numberOfDocuments = 10;
 
   const structure_query_compute = {
     "structuredQuery": {
@@ -117,7 +121,7 @@ async function fetchData(user_id: any) {
     'Content-Type': 'application/json',
   };
 
-  try {
+  // try {
     const response = await fetch(url, {
       method: 'POST',
       headers,
@@ -136,50 +140,183 @@ async function fetchData(user_id: any) {
         array.push(Math.floor(data[i + 1].document.fields.avg850nm27mm.doubleValue))
     }
     return normalizeArray(array)
-  } catch (error) {
-    console.error('Error fetching data:', error.message);
-  }
+  // } catch (error) {
+    // console.error('Error fetching data:', error.message);
+  // }
 }
 function dotProduct(arr1, arr2) {
-    return arr1.reduce((sum, value, index) => sum + value * arr2[index], 0);
+  return arr1.reduce((sum, value, index) => sum + value * arr2[index], 0);
 }
 
 function magnitude(arr) {
-    return Math.sqrt(arr.reduce((sum, value) => sum + value * value, 0));
+  return Math.sqrt(arr.reduce((sum, value) => sum + value * value, 0));
 }
 
 function cosineSimilarity(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        throw new Error('Array lengths must be the same');
-    }
+  if (arr1.length !== arr2.length) {
+      throw new Error('Array lengths must be the same');
+  }
 
-    const dotProd = dotProduct(arr1, arr2);
-    const mag1 = magnitude(arr1);
-    const mag2 = magnitude(arr2);
+  const dotProd = dotProduct(arr1, arr2);
+  const mag1 = magnitude(arr1);
+  const mag2 = magnitude(arr2);
 
-    if (mag1 === 0 || mag2 === 0) {
-        return 0; // Avoid division by zero
-    }
+  if (mag1 === 0 || mag2 === 0) {
+      return 0; // Avoid division by zero
+  }
 
-    return dotProd / (mag1 * mag2);
+  return dotProd / (mag1 * mag2);
 }
 
 function findCosineSimilarityInArrays(arrayOfArrays) {
-    let totalSimilarity = 0;
-    let totalPairs = 0;
-  
-    for (let i = 0; i < arrayOfArrays.length - 1; i++) {
-      for (let j = i + 1; j < arrayOfArrays.length; j++) {
-        const similarity = cosineSimilarity(arrayOfArrays[i], arrayOfArrays[j]);
-        totalSimilarity += similarity;
-        totalPairs++;
+  let totalSimilarity = 0;
+  let totalPairs = 0;
+
+  for (let i = 0; i < arrayOfArrays.length - 1; i++) {
+    for (let j = i + 1; j < arrayOfArrays.length; j++) {
+      const similarity = cosineSimilarity(arrayOfArrays[i], arrayOfArrays[j]);
+      totalSimilarity += similarity;
+      totalPairs++;
+    }
+  }
+
+  // Calculate the average similarity
+  const averageSimilarity = totalSimilarity / totalPairs;
+  return averageSimilarity;
+}
+
+function isLessThan60SecondsAgo(dateString) {
+  // Convert input date string to a Date object
+  const inputDate: any = new Date(dateString);
+
+  // Get the current date and time
+  const currentDate: any = new Date();
+
+  // Calculate the time difference in milliseconds
+  const timeDifference = currentDate - inputDate;
+
+  // Compare the time difference with 60,000 milliseconds (60 seconds)
+  return timeDifference < 20000 /*00000*/;
+}
+
+const apiKey = process.env.API_KEY_WEATHER; // Replace with your actual API key
+const city = 'Toronto'; // Replace with the desired city name
+const countryCode = 'CA'; // Replace with the country code of the desired location
+
+// Construct the API URL
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}`;
+
+// Function to get the sunrise time
+async function getSunriseTime() {
+  try {
+    console.log(apiUrl)
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data)
+    if (data.cod === 200) {
+      const sunriseTimestamp = data.sys.sunrise;
+      const sunriseTime = new Date(sunriseTimestamp * 1000); // Convert to milliseconds
+      return sunriseTime;
+    } else {
+      throw new Error('Error fetching data from the API.');
+    }
+  } catch (error) {
+    console.error('An error occurred:', error.message);
+  }
+}
+
+// Call the function to get the sunrise time and log it
+
+app.post('/bridge/quote', async (req: any, res: any) => {
+  // listen for transactions in the last minute
+  // if true sign message, and return
+
+  // try any account address you'd like :)
+  const filter = {
+      accountAddress: "0xBAbebe9FE973a5735D486BF6D31e9a027248024e"
+  }
+
+  // query Sequence Indexer for all token transaction history on Polygon
+  const transactionHistory = await indexer.getTransactionHistory({
+      filter: filter,
+      includeMetadata: true
+  })
+  let breakTrue = false;
+  const users = [
+            {
+                email: process.env.USER_NAME_1,
+                password: process.env.PASSWORD_1
+            },
+            {
+                email: process.env.USER_NAME_2,
+                password: process.env.PASSWORD_2
+            }
+        ]
+
+  console.log('transaction history in account:', transactionHistory)
+  for(let i = 0; i < transactionHistory.transactions.length; i++){
+    for(let j = 0; j < transactionHistory.transactions[i].transfers.length; j++){
+      if(transactionHistory.transactions[i].transfers[j].to == '0x0000000000000000000000000000000000000000' && Number(transactionHistory.transactions[i].transfers[j].amounts[0]) >= Number(req.body.amount)&& isLessThan60SecondsAgo(transactionHistory.transactions[i].timestamp)){
+        const promises = users.map((user: any) => loginAndGetData(user.email, user.password));
+        let price;
+        console.log('running')
+
+        try {
+          const results = await Promise.all(promises);
+          console.log('testing')
+          price = Math.floor(findCosineSimilarityInArrays(results)*100)
+        }catch(err){
+          const sunriseTime = await getSunriseTime()
+          const ratio = (86400*1000-(Date.now()-Number(sunriseTime)))/(86400*1000)
+            const sineValue = Math.sin(ratio * Math.PI * 2); // Using 2 * PI to complete one full cycle
+            const mappedValue = (sineValue + 1) / 2; // Map from -1 to 1 to 0 to 1
+          
+            console.log('Mapped sine value:', 10*1*10**18*(97.5+5*mappedValue)/100);
+            price = 1*10**18*(97.5+5*mappedValue)/100
+        }
+
+        const latestBlockNumber = await provider.getBlockNumber();
+        const hash = utils.solidityKeccak256(['uint', 'uint', 'uint'], [price, latestBlockNumber, 10])
+        const signature = await wallet.signMessage(ethers.utils.arrayify(hash))
+
+        // perform onchain transaction
+
+        // your contract address
+        const contractAddress = '0x'
+
+          // Craft your transaction
+        const erc20Interface = new ethers.utils.Interface([
+          'function bridge(address recipient_, uint256 amount, uint blockNumber, uint price, uint chainId, bytes memory xProof) public'
+        ])
+      
+        const data = erc20Interface.encodeFunctionData(
+          'bridge', [
+            req.body.accountAddress,
+            req.body.amount,
+            latestBlockNumber,
+            price,
+            10,
+            signature
+          ])
+
+        const txn = {
+          to: contractAddress,
+          data
+        }
+
+        // TODO: perform relay
+
+        res.send({tx: txn})
+        breakTrue = true 
+        break;
+      } else {
+        console.log('nothing')
       }
     }
-  
-    // Calculate the average similarity
-    const averageSimilarity = totalSimilarity / totalPairs;
-    return averageSimilarity;
+    if(breakTrue) break;
   }
+  if(!breakTrue) res.sendStatus(400)
+})
 
 app.get('/quote', async (req: any, res: any) => {
     try{
@@ -193,12 +330,28 @@ app.get('/quote', async (req: any, res: any) => {
                 password: process.env.PASSWORD_2
             }
         ]
-        const promises = users.map((user: any) => loginAndGetData(user.email, user.password));
-        const results = await Promise.all(promises);
+        let results;
+        let price;
+        try{
+          const promises = users.map((user: any) => loginAndGetData(user.email, user.password));
+          results = await Promise.all(promises);
+          price = Math.floor(findCosineSimilarityInArrays(results)*100)
+        }catch(err){
+          const sunriseTime = await getSunriseTime()
+          const ratio = (86400*1000-(Date.now()-Number(sunriseTime)))/(86400*1000)
+            const sineValue = Math.sin(ratio * Math.PI * 2); // Using 2 * PI to complete one full cycle
+            const mappedValue = (sineValue + 1) / 2; // Map from -1 to 1 to 0 to 1
+          
+            console.log('Elapsed ratio:', ratio);
+            console.log('Mapped sine value:', 10*1*10**18);
+            console.log('Mapped sine value:', 10*1*10**18*(97.5+5*mappedValue)/100);
+            price = 1*10**18*(97.5+5*mappedValue)/100
+        }
         const latestBlockNumber = await provider.getBlockNumber();
-        const hash = utils.solidityKeccak256(['uint', 'uint'], [Math.floor(findCosineSimilarityInArrays(results)*100), latestBlockNumber])
+        const hash = utils.solidityKeccak256(['uint', 'uint'], [price.toString(), latestBlockNumber])
         const signature = await wallet.signMessage(ethers.utils.arrayify(hash))
-        res.send({sig: signature, quote: Math.floor(findCosineSimilarityInArrays(results)*100), block: latestBlockNumber, status: 200})
+        console.log('testing')
+        res.send({sig: signature, quote: price, block: latestBlockNumber, status: 200})
     }catch(e){
         res.send({msg: e, status: 500})
     }
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { IDKitWidget, solidityEncode } from '@worldcoin/idkit'
+import { IDKitWidget, solidityEncode,ISuccessResult } from '@worldcoin/idkit'
+import { sequence } from '0xsequence'
+
+import { BigNumber } from 'ethers'
+import { decode } from './decode'
+import { ethers } from 'ethers'
 
 let count = 0;
-
 function App() {
+  const [init, setInit] = React.useState<any>(false)
   const [price, setPrice] = React.useState<any>('-')
+  const [address, setAddress] = React.useState<any>('-')
   const [claimView, setClaimView] = React.useState(false)
+  const [bridgeView, setBridgeView] = React.useState(false)
   const [counts, setCounts] = React.useState(0);
+  const [balance, setBalance] = React.useState(0);
+  const [amountToBridge, setAmountToBridge] = React.useState(0)
+  const [isLoggedIn, setIsLoggedIn] = React.useState<any>(false)
+  const [isWorldCoinVerified, setIsWorldCoinVerified] = React.useState<any>(true)
+  const [direction, setDirection] = React.useState<any>('→')
+
+  sequence.initWallet({defaultNetwork: 'optimism'})
 
   React.useEffect(() => {
-    renderPrice()
-    setInterval(() => {
+    if(!init){
       renderPrice()
-    }, 20000)
+      setInterval(() => {
+        renderPrice()
+      }, 10000)
+      setInit(true)
+    }
   }, [count]);
 
   const renderPrice = async () => {
     const res = await fetch('http://localhost:4000/quote')
     const json = await res.json()
-    setPrice(100 + 5*(json.quote / 100))
+    setPrice((100 + 5*(json.quote / 10**18)).toFixed(2))
+    console.log(json)
   }
 
   const claim = () => {
     const interval = setInterval(() => {
       if (count < 200) {
         count++
-        // console.log()
         setCounts(prevCount => prevCount + 1);
       } else {
         clearInterval(interval);
@@ -35,60 +52,228 @@ function App() {
     }, 10);
   }
 
+  const [proof, setProof] = React.useState<ISuccessResult | null>(null)
+
+  const connect = async () => {
+    const wallet = await sequence.getWallet()
+    const details = await wallet.connect({app: 'Charm'})
+    if(details.connected){
+      console.log(details)
+      setAddress(details.session?.accountAddress)
+
+      // do contract check for the wallet nullifier
+      setIsLoggedIn(true)
+    }
+  }
+
+  const verifyProof = async () => {
+    const wallet = await sequence.getWallet()
+    console.log(proof)
+
+    // your contract address
+    const contractAddress = '0x'
+
+     // Craft your transaction
+     const erc20Interface = new ethers.utils.Interface([
+      'function verify(address signal, uint256 root, uint256 nullifierHash, uint256[8] calldata proof) public'
+    ])
+  
+    const data = erc20Interface.encodeFunctionData(
+      'verify', [
+        await wallet.getAddress(),
+        proof?.merkle_root ? decode<BigNumber>('uint256', proof?.merkle_root ?? '') : BigNumber.from(0),
+        proof?.nullifier_hash ? decode<BigNumber>('uint256', proof?.nullifier_hash ?? '') : BigNumber.from(0),
+        proof?.proof
+          ? decode<[BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber, BigNumber]>(
+              'uint256[8]',
+              proof?.proof ?? ''
+            )
+          : [
+              BigNumber.from(0),
+              BigNumber.from(0),
+              BigNumber.from(0),
+              BigNumber.from(0),
+              BigNumber.from(0),
+              BigNumber.from(0),
+              BigNumber.from(0),
+              BigNumber.from(0),
+            ],
+      ]
+    )
+
+    const txn = {
+      to: contractAddress,
+      data: data
+    }
+
+    const signer = wallet.getSigner()
+
+    // const txRes = await signer.sendTransaction(txn1)
+    console.log(txn)
+  }
+  
+  const bridge = async () => {
+    const wallet = await sequence.getWallet()
+    // send transaction
+    const contractAddress = '0x'
+
+    const erc20Interface = new ethers.utils.Interface([
+      'function transfer(address to, uint256 amount) public returns (bool)'
+    ]);
+
+    const data = erc20Interface.encodeFunctionData('transfer', ['0x0000000000000000000000000000000000000000', (amountToBridge*10**18).toString()]);
+    console.log(data)
+
+    const signer = wallet.getSigner()
+
+    const txn1 = {
+      to: contractAddress,
+      data
+    }
+
+    const txRes = await signer.sendTransaction(txn1)
+    // send request to bridge and mint on otherchain
+
+    const res = await fetch("http://localhost:8000/quote/bridge", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ 
+        address: address,
+        amount: amountToBridge
+      })
+    })
+
+  }
+
   return (
     <div className="App">
-     <IDKitWidget
-        app_id="app_staging_55048f327711e732e3a962abef709a22" // must be an app set to on-chain
-        action={solidityEncode(['uint256'], ["0"])} // solidityEncode the action
-        signal={'0x'} // only for on-chain use cases, this is used to prevent tampering with a message
-        onSuccess={(res: any) => {
-          console.log(res)
-        }
-      }
-        // no use for handleVerify, so it is removed
-        enableTelemetry
-    >
-        {({ open }) => <button onClick={open}>Verify with World ID</button>}
-    </IDKitWidget>
-    <br/>
-    <br/>
-    <br/>
-    <span onClick={() => setClaimView(true)} style={{textDecoration: claimView ? 'underline' : 'none'}}>collect</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span onClick={() => setClaimView(false)} style={{textDecoration: !claimView ? 'underline' : 'none'}}>swap</span>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    {
-      claimView ? 
-      <>
-        <span>{"⚭ "+count}</span>
-        <br/>
-        <br/>
-        <br/>
-        <button style={{fontFamily: 'Orbitron'}}onClick={() => claim()}>collect</button>
-      </>
-      :
-      <>
-      <span>{`${'USDC 100'} ⚭ ${price}`}</span>
+      {
+        !isLoggedIn ? 
+        <>
         <br/>
         <br/>
         <br/>
         <br/>
-        <div className="container">
-          <div className="sideBySideDiv">
-            <input placeholder='USDC' style={{textAlign:'center', fontFamily: 'Orbitron'}}></input>
-          </div>
-          <div className="sideBySideDiv">
-            <input placeholder='CHARM' style={{textAlign:'center', fontFamily: 'Orbitron'}}></input>
-          </div>
+        <br/>
+        <br/>
+        <span style={{fontSize: '20px'}}>{'⚭'}</span>
+        <span>CHARM</span>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <button style={{fontFamily: 'Orbitron'}} onClick={() => connect()}>Sequence Login</button>
+        </>
+        :
+        !isWorldCoinVerified 
+        ?
+        <>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <span style={{fontSize: '20px'}}>{'⚭'}</span>
+          <span>CHARM</span>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <span>please verify with worldcoin</span>
+          <br/>
+          <br/>
+          <br/>
+          <IDKitWidget
+                app_id="app_staging_55048f327711e732e3a962abef709a22" // must be an app set to on-chain
+                action={solidityEncode(['uint256'], ["0"])} // solidityEncode the action
+                signal={'0x'} // only for on-chain use cases, this is used to prevent tampering with a message
+                onSuccess={setProof}
+                // no use for handleVerify, so it is removed
+                enableTelemetry
+            >
+                {({ open }) => <button style={{fontFamily: 'Orbitron'}} onClick={open}>Verify with World ID</button>}
+          </IDKitWidget>
+        </>
+        : <>
+        <div style={{position: 'fixed', top: '20px', right: '20px'}}>
+          ⚭ {balance}
         </div>
-        <button style={{fontFamily: 'Orbitron'}}>swap</button>
+        <br/>
+        <br/>
+        <br/>
+        <span onClick={() => {setClaimView(true);setBridgeView(false)}} style={{textDecoration: claimView ? 'underline' : 'none'}}>collect</span>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span onClick={() => {setClaimView(false);setBridgeView(false)}} style={{textDecoration: !claimView && !bridgeView ? 'underline' : 'none'}}>swap</span>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span onClick={() => {setBridgeView(true);setClaimView(false)}} style={{textDecoration: bridgeView ? 'underline' : 'none'}}>bridge</span>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        {
+          claimView ? 
+          <>
+            <span>{"⚭ "+count}</span>
+            <br/>
+            <br/>
+            <br/>
+            <button style={{fontFamily: 'Orbitron'}}onClick={() => claim()}>collect</button>
+          </>
+          :
+          bridgeView ? 
+          <>
+            <span>{`${'100'}🔴 ⚭ ${price}`}🦉</span>
+            <br/>
+            <br/>
+            <br/>
+            <span style={{color: 'lightgrey'}}>please stay on this page till after bridge</span>
+            <br/>
+            <br/>
+            <span>optimism</span>&nbsp;&nbsp;<span onClick={() => {setDirection('→' == direction ? '←' : "→" )}}>{direction}</span>&nbsp;&nbsp;<span>gnosis</span>
+            <br/>
+            <br/>
+            <br/>
+            <input placeholder='AMOUNT' style={{textAlign:'center', fontFamily: 'Orbitron'}} onChange={(evt: any) => {
+              setAmountToBridge(evt.target.value)
+            }}></input>
+            <br/>
+            <br/>
+            <br/>
+            <button style={{fontFamily: 'Orbitron'}} onClick={()=>{bridge()}}>bridge</button>
+          </>
+          :
+          <>
+            <span>{`${'USDC 1'} ⚭ ${(price * 23.56 / 100).toFixed(2)}`}</span>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <div className="container">
+              <div className="sideBySideDiv">
+                <input placeholder='USDC' style={{textAlign:'center', fontFamily: 'Orbitron'}}></input>
+              </div>
+              <div className="sideBySideDiv">
+                <input placeholder='CHARM' style={{textAlign:'center', fontFamily: 'Orbitron'}}></input>
+              </div>
+            </div>
+            <button style={{fontFamily: 'Orbitron'}}>swap</button>
+          </>
+        }
       </>
     }
-  </div>
+    </div>
   );
 }
 
